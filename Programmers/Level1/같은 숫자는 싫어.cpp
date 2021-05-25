bc7de36e66a25813f3d3f04f1f95344de83baed1
riyenas0925 <riyenas0925@gmail.com>
2021-05-25 19:42:51 +0900

    Prog : 같은 숫자는 싫어

---
+++ Programmers/Level1/같은 숫자는 싫어.cpp
@@ -0,0 +1,11 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr) 
+{   
+    arr.erase(unique(arr.begin(), arr.end()), arr.end());
+    return arr;
+}
