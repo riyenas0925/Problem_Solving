8d54b13fddd83022b514aee842c9ff40be75f1f8
riyenas0925 <riyenas0925@gmail.com>
2021-06-06 21:03:50 +0900

    Prog : 3진법 뒤집기 (vector 사용)

--- Programmers/Level1/3진법 뒤집기.cpp
+++ Programmers/Level1/3진법 뒤집기.cpp
@@ -5,19 +5,19 @@
 using namespace std;
 
 int solution(int n) {
-    int answer = 0;
-    int temp = 1;
-    string num = "";
+    int answer = 0, k = 1;
+    vector<int> num;
     
-    for(int i = 0; n > 0; i++) {
-        num += to_string(n % 3);
+    while(n > 0) {
+        num.push_back(n % 3);
         n /= 3;
     }
-    
-    for(int i = 0; i < num.length(); i++) {
-        answer += (num[num.length() - i -1] - '0') * temp;
-        temp *= 3;
+
+    while(!num.empty()) {
+        answer += k * num.back();
+        num.pop_back();
+        k *= 3;
     }
-    
+
     return answer;
 }
